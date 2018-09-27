b2eb4444b2d5d8b1101c599b01997de7e0d48807
Boris De Torck <boris.detorck.y3443@student.hogent.be>
2018-09-27 12:14:52 +0200

    Add project files.

---
+++ Banking/Models/Domain/BankAccount.cs
@@ -0,0 +1,77 @@
+﻿using System;       //imports zoals java
+using System.Collections.Generic;
+using System.Text;
+
+//bij compilatie wordt dll aangemaakt in assembly
+
+namespace Banking.Models.Domain
+{
+    public class BankAccount
+    {
+
+        private decimal _balance;
+        public const decimal withdrawCost = 0.10M;
+
+        /* jave methode setter en getter
+         
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        private void SetBalance(decimal value)
+        {
+            _balance = value;
+        } */
+
+        /*automatic property Balance (hier maakt property setter/getter voor balance var
+         vb. gebruik van de property
+              ba.Balance = 20; -> roept setter
+              ba.Balance; -> roept getter aan
+
+         public decimal Balance { get; private set; }
+         */
+
+        //full property Balance(kan setter/getter volledig definieren) tesst 
+        public decimal Balance
+        {
+            get { return _balance; }    
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid values");
+                }
+                else
+                {
+                    _balance = value;   //value bevat 20 uit vb
+                }
+            }
+        }
+
+        public string AccountNumber { get; set; }
+
+        #region Constructors
+        public BankAccount(string accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber)  // andere constructor aanroepen
+        {
+            Balance = balance;
+        } 
+        #endregion
+
+        public void Deposit(decimal amount, int nrOfTimes = 1)  // door waarde toe te kennen aan parameter wordt het een optionele parameter --> waarde meegegeven neem die waarde, anders neem default waarde
+        {
+            Balance += amount * nrOfTimes;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount + withdrawCost;
+        }
+
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,28 @@
+﻿using Banking.Models.Domain;
+using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myBA = new BankAccount("123-123123123-23");
+            BankAccount otherBA = new BankAccount("456-456456456-45", 1000);
+
+            Console.WriteLine($"Balance of otherBa is currently: {otherBA.Balance} Euro");
+
+            Console.WriteLine($"withdraw cost is currently: {BankAccount.withdrawCost} Euro");
+
+            Console.WriteLine($"Balance is currently: {myBA.Balance} Euro");
+            myBA.Deposit(100);
+            myBA.Deposit(500,2);
+            Console.WriteLine($"Balance is currently: {myBA.Balance} Euro");
+            myBA.Withdraw(50);
+            Console.WriteLine($"Balance is currently: {myBA.Balance} Euro");
+            myBA.Withdraw(10);
+            Console.WriteLine($"Balance is currently: {myBA.Balance} Euro");
+            Console.ReadKey();
+        }
+    }
+}
